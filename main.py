abb3b7f3451637bb852fd0e2239d5ca713c4091a
idontmakestuff <145033745+idontmakestuff@users.noreply.github.com>
2024-02-20 15:34:29 +0000

    fixing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-inventory=["sword","shield","-","-"]
+inventory=["-","-","-","-"]
 map=[["f1","f2","f3","f4","f5","f6","roof"]
      ,["enterance","shop","training","outlook","street","town","home"]]
 items=[["","","","","","",""]
@@ -6,13 +6,15 @@ items=[["","","","","","",""]
 playerx=6
 playery=1
 
-def move(direction, playerx, playery):
+def move(direction):
+    global playerx
+    global playery
     if direction=="west":
         if playerx>0:
             playerx-=1  
     elif direction=="east":
         if playerx<6:
-            player+=1
+            playerx+=1
     elif direction=="north":
         if map[playery][playerx]=="enterance":
             playery-=1
@@ -23,6 +25,9 @@ def move(direction, playerx, playery):
         print("Cannot move there!")
         print(direction)
     print("You are at "+str(map[playery][playerx])+".")
+    print (playerx)
+    print(playery)
+
 
 def upgrade (inventory,size):
     newinventory=[]
@@ -34,15 +39,37 @@ def upgrade (inventory,size):
                 newinventory.append("-")
     return newinventory
 
+def pickup(item):
+    global inventory
+    for i in inventory:
+        if i=="-":
+            if item not in inventory:
+                inventory[i]=item
+        #     else:
+        #         print("already found")
+        # else:
+        #     print("no space")
+    print(inventory)
+    return inventory
+
 game=True
 
 while game:
     text=input("> ")
     command=text.split()
     if "move" in text:
-        move(command[1],playerx,playery)
+        move(command[1])
     elif "look" in text:
         print("You look around, and find a "+items[playery][playerx]+".")
+    elif "pickup" in text:
+        pickup(items[playery][playerx])
+    elif "inventory" in text:
+        print (inventory)
+    elif "buy" in text:
+        if "coin" in inventory:
+            if playery==1 and playerx==1:
+                upgrade(inventory,6)
+                
 # move("west", playerx, playery)
 # move("east", playerx, playery)
 # print (upgrade(inventory,6))
